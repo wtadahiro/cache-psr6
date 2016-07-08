d892ed24efeb2b3737808f33ec88eddbe50458d7
tadahiro.wada <tadahiro.wada@d8aspring.com>
2016-07-08 21:17:46 +0900

    add a skeleton test class

---
+++ tests/CacheItemTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wtadahiro\tests;
+
+use Wtadahiro\Cache\CacheItem;
+
+class CacheItemTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetKey()
+    {
+        $cache = new CacheItem();
+
+    }
+}
